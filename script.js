62600b292b4eb9ebef27c8258c4458a270c85dd8
salonikansal789 <94972204+salonikansal789@users.noreply.github.com>
2024-12-05 23:04:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 // your code here
+function formSubmit(){
+	 const nameValue = document.getElementById("name").value;
+      const yearValue = document.getElementById("year").value;
+	      const queryString = `?name=${encodeURIComponent(nameValue)}&year=${encodeURIComponent(yearValue)}`;
+	document.getElementById('url').textContent=`https://localhost:8080/${queryString}`;
+	
+}
