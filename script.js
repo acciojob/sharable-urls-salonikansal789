142773c734365ad499cdc03644f2244efd15f792
salonikansal789 <94972204+salonikansal789@users.noreply.github.com>
2024-12-05 23:10:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 // your code here
-function formSubmit(){
+function formSubmit(event){
+	 event.preventDefault();
 	 const nameValue = document.getElementById("name").value;
       const yearValue = document.getElementById("year").value;
-	      const queryString = `?name=${encodeURIComponent(nameValue)}&year=${encodeURIComponent(yearValue)}`;
+	const queryString = `?name=${encodeURIComponent(nameValue)}&year=${encodeURIComponent(yearValue)}`;
 	document.getElementById('url').textContent=`https://localhost:8080/${queryString}`;
 	
 }
